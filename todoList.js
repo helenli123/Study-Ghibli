786fbae248bc298cf1c20d92bf855dfd8a303ff5
Glitch (glitch-blank-website) <none>
2022-01-07 22:33:50 +0000

    ⛹🐡 Checkpoint ./style.css:40755028/11068 ./todoList.js:40754953/317 ./index.html:40755028/487 ./testTodoList.html:40754953/19

--- todoList.js
+++ todoList.js
@@ -1,15 +1,29 @@
 const listsContainer = document.querySelector('[data-lists]');
 
-let lists = [];
+let lists = [{
+  id: 1,
+  name: 'name'
+}, {
+  id: 2,
+  name: 'todo'
+}];
 
 function render() {
   clearElement(listsContainer);
   lists.forEach(list => {
     const listElement = document.createElement('li');
+    listElement.dataset.listId = list.id;
     listElement.classList.add("list-name");
+    listElement.innerText = list.name;
+    listsContainer.appendChild(listElement);
   })
 }
 
 function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
 
 }
+
+render();
