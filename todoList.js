71e95defc88f65c1ef28e547cd14dcc320de9cf8
Glitch (glitch-blank-website) <none>
2022-01-07 22:09:48 +0000

    🏂🦃 Checkpoint ./testTodoList.html:40754953/3485 ./todoList.css:40754953/4574 ./directory.html:40754953/69 ./study1.html:40754953/74 ./todoList.js:40754953/116 ./style.css:40754953/53:40755028/200

--- todoList.js
+++ todoList.js
@@ -1,3 +1,11 @@
 const listsContainer = document.querySelector('[data-lists]');
 
 let lists = [];
+
+function render() {
+  clearElement(listsContainer);
+}
+
+function clearElement(element) {
+
+}
