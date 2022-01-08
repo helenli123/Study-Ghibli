130630a4bb6b570d19b2f9d807000ffaf66749d5
Glitch (glitch-blank-website) <none>
2022-01-08 01:30:22 +0000

    🚠🌓 Checkpoint ./Spirited_Away.css:40755028/8616 ./todoList.js:40754953/1311 ./music.js:40755028/2466 ./Spirited_Away.html:40755028/9 ./testTodoList.html:40754953/73

--- todoList.js
+++ todoList.js
@@ -7,6 +7,8 @@ const listTitleElement = document.querySelector('[data-list-title]');
 const listCountElement = document.querySelector('[data-list-count]');
 const tasksContainer = document.querySelector('[data-tasks]');
 const taskTemplate = document.getElementById('task-template');
+const newTaskForm = document.querySelector('[data-new-task-form]');
+const newTaskInput = document.querySelector('[data-new-task-input]');
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
@@ -16,6 +18,13 @@ let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 //allows for a list to be selected
 listsContainer.addEventListener('click', e => {
   if (e.target.tagName.toLowerCase() === 'li') {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+})
+
+tasksContainer.addEventListener('click', e => {
+  if (e.target.tagName.toLowerCase() === 'input') {
     selectedListId = e.target.dataset.listId;
     saveAndRender();
   }
@@ -38,9 +47,25 @@ newListForm.addEventListener('submit', e => {
   saveAndRender();
 })
 
+//add task item to the list of tasks
+newTaskForm.addEventListener('submit', e => {
+  e.preventDefault(); //prevent page from automatically reloading
+  const taskName = newTaskInput.value;
+  if (taskName == null || taskName === '') return  //make sure the user actually typed something
+  const task = createTask(taskName);
+  newTaskInput.value = null;  //clear form
+  const selectedList = lists.find(list => list.id === selectedListId);
+  selectedList.tasks.push(task);
+  saveAndRender();
+})
+
 //create item in the list of lists (my lists)
 function createList(name) {
   return {id: Date.now().toString(), name: name, tasks: []};
+}
+
+function createTask(name) {
+  return {id: Date.now().toString(), name: name, complete: false};
 }
 
 function saveAndRender() {
